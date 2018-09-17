74efb361c4abb92c100c205ca62606bd1186031c
Borna Ljudevit Matijanic <borna.matijanic@gmail.com>
2018-09-17 14:54:33 +0200

    Persist and flush user data to database

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -1,12 +1,13 @@
 <?php
   namespace App\Controller;
 
+  use App\Entity\Data;
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
   use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
-  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
-  class VisitorController {
+  class VisitorController extends Controller {
     /**
      * @Route("/")
      * @param Request $request
@@ -14,6 +15,16 @@
      */
     public function index(Request $request) {
       $ip = $request->getClientIp();
-      return new Response($ip);
+      $time = new \DateTime();
+
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $data = new Data();
+      $data->setIp($ip);
+      $data->setTime($time);
+
+      $entityManager->persist($data);
+      $entityManager->flush();
+      return new Response("We are not monitoring you :)");
     }
   }
