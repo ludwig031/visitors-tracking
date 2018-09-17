5c25529fe2697a3e7dc0695194b30c006da502bb
Borna Ljudevit Matijanic <borna.matijanic@gmail.com>
2018-09-17 11:59:19 +0200

    Set routing trough annotations

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -3,8 +3,15 @@
 
   use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
   class VisitorController {
+    /**
+     * @Route("/")
+     * @param Request $request
+     * @return Response
+     */
     public function index(Request $request) {
       $ip = $request->getClientIp();
       return new Response($ip);
