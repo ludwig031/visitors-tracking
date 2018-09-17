08a099e9d1eb8ba1a4a00a3ecb41bcf1fc775f0c
Borna Ljudevit Matijanic <borna.matijanic@gmail.com>
2018-09-17 15:12:46 +0200

    Add host and user agent to user data

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -15,16 +15,22 @@
      */
     public function index(Request $request) {
       $ip = $request->getClientIp();
+
       $time = new \DateTime();
-
-
+      dump($request->server);
+      $host = $request->server->get('HTTP_HOST');
+      $userAgent = $request->server->get('HTTP_USER_AGENT');
       $entityManager = $this->getDoctrine()->getManager();
+
       $data = new Data();
       $data->setIp($ip);
       $data->setTime($time);
+      $data->setHost($host);
+      $data->setUserAgent($userAgent);
 
       $entityManager->persist($data);
-      $entityManager->flush();
-      return new Response("We are not monitoring you :)");
+      $entityManager->flush();  
+
+      return new Response("<html><body>We are not monitoring you</body></html>");
     }
   }
