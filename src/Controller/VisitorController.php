e602ab11d05f278ac691bd5989d1f0f5f1e3bd4f
Borna Ljudevit Matijanic <borna.matijanic@gmail.com>
2018-09-17 16:47:27 +0200

    Add token to cookie to check user

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -17,11 +17,10 @@
      */
     public function index(Request $request) {
       $ip = $request->getClientIp();
-
       $time = new \DateTime();
-      dump($request->server);
       $host = $request->server->get('HTTP_HOST');
       $userAgent = $request->server->get('HTTP_USER_AGENT');
+      $token = $this->Token();
       $entityManager = $this->getDoctrine()->getManager();
 
       $data = new Data();
@@ -29,10 +28,32 @@
       $data->setTime($time);
       $data->setHost($host);
       $data->setUserAgent($userAgent);
+      $data->setToken($token);
 
       $entityManager->persist($data);
       $entityManager->flush();
 
       return $this->render('base.html.twig');
+    }
+
+    public function Token(){
+      if(!isset($_COOKIE['token'])) {
+        $randomString = $this->RandomString();
+        setcookie('token', $randomString);
+        return $randomString;
+      } else {
+        return $_COOKIE['token'];
+      }
+    }
+
+    public function RandomString()
+    {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < 10; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+      }
+      return $randomString;
     }
   }
